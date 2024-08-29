af30877cb28a1626f190581e8eecc79f6a1fa01b
Fa-na-ali <145117093+Fa-na-ali@users.noreply.github.com>
2024-08-29 18:41:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function show()
+{
+document.getElementById("show-div").style.display = "block";
+
+document.getElementById("plus").innerHTML = "&#45;"
+
+}
+function showBlock()
+{
+    document.getElementById("show-div-ele").style.display = "block";
+    document.getElementById("plus-p").innerHTML = "&#45;"
+}
